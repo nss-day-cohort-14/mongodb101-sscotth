2b9768eb0c90c52fbc4747842e2d20e3d1fabfe3
Scott Humphries <git@sscotth.io>
2016-09-13 10:43:55 -0500

    done

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 'use strict'
 
-console.log('MongoDB 101')
+const { MongoClient: { connect } } = require('mongodb')
+
+const { argv: [,, ...filter] } = process
+
+const MONGODB_URL = 'mongodb://localhost:27017/test'
+
+const name = RegExp(`^${filter.join(' ')}`, 'i')
+
+connect(MONGODB_URL)
+  .then(db => {
+    db.collection('restaurants')
+      .find({ name })
+      .sort({ name: 1 })
+      // .toArray()
+      // .then((restaurants) => {
+      //   restaurants.forEach(restaurant => {
+      //     if (restaurant.name) {
+      //       console.log(restaurant.name)
+      //     }
+      //   })
+      // })
+      // .then(() => db.close())
+      .forEach(restaurant => {
+        if (restaurant.name) {
+          console.log(restaurant.name)
+        }
+      }, () => db.close())
+  })
+  .catch(console.error)
